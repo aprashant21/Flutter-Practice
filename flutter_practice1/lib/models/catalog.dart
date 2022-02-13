c6160cec90250829fc666a29b57562c4cf2180e0
aprashant21 <aprashant21@tbc.edu.np>
2022-02-14 01:51:08 +0545

    Local File Json Mapping and Data Class Generator

--- flutter_practice1/lib/models/catalog.dart
+++ flutter_practice1/lib/models/catalog.dart
@@ -1,16 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class CatalogModel {
-  static final items = [
-    Item(
-        id: 1,
-        name: "Amir Khan",
-        desc: "Amir Khan is a great indian actor.",
-        price: 200,
-        color: "#33505a",
-        image:
-            "https://static.abplive.com/wp-content/uploads/2020/05/13042222/Aamir-Khan.jpg?impolicy=abp_cdn&imwidth=720")
-  ];
+  static List<Item> items = [];
 }
 
 class Item {
@@ -18,14 +11,81 @@ class Item {
   final String name;
   final String desc;
   final num price;
-  final String color;
   final String image;
 
-  Item(
-      {required this.id,
-      required this.name,
-      required this.desc,
-      required this.price,
-      required this.color,
-      required this.image});
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.image,
+  });
+
+ 
+
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? image,
+  }) {
+    return Item(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      image: image ?? this.image,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map['id']?.toInt() ?? 0,
+      name: map['name'] ?? '',
+      desc: map['desc'] ?? '',
+      price: map['price'] ?? 0,
+      image: map['image'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) => Item.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+  
+    return other is Item &&
+      other.id == id &&
+      other.name == name &&
+      other.desc == desc &&
+      other.price == price &&
+      other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+      name.hashCode ^
+      desc.hashCode ^
+      price.hashCode ^
+      image.hashCode;
+  }
 }
--- flutter_practice1/lib/pages/home.dart
+++ flutter_practice1/lib/pages/home.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_practice1/models/catalog.dart';
 import 'package:flutter_practice1/widgets/drawer.dart';
 import 'package:flutter_practice1/widgets/item.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
-  final String name = "Prashant Adhikari";
-  final int age = 22;
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString("res/json/data.json");
+    final decodedData = jsonDecode(catalogJson);
+    var booksData = decodedData["books"];
+    CatalogModel.items =
+        List.from(booksData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(15, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Smart Study"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: dummyList.length,
+        child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)? ListView.builder(
+            itemCount: CatalogModel.items.length,
             itemBuilder: (context, index) {
-              return ItemWidget(item: dummyList[index]);
-            }),
+              return ItemWidget(item: CatalogModel.items[index]);
+            }): Center(
+              child: CircularProgressIndicator(),
+            ),
       ),
       drawer: MyDrawer(),
     );
