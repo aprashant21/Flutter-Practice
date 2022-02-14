038a4bf2176810304ebb1c34842a08ac11bad8d0
aprashant21 <aprashant21@tbc.edu.np>
2022-02-14 09:49:49 +0545

    GridView builder and GridTile

--- flutter_practice1/lib/pages/home.dart
+++ flutter_practice1/lib/pages/home.dart
@@ -19,7 +19,7 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
-    await Future.delayed(Duration(seconds: 2));
+    // await Future.delayed(Duration(seconds: 2));
     final catalogJson = await rootBundle.loadString("res/json/data.json");
     final decodedData = jsonDecode(catalogJson);
     var booksData = decodedData["books"];
@@ -36,13 +36,38 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items!=null && CatalogModel.items.isNotEmpty)? ListView.builder(
-            itemCount: CatalogModel.items.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(item: CatalogModel.items[index]);
-            }): Center(
-              child: CircularProgressIndicator(),
-            ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16),
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      header: Container(
+                        child: Text(item.name, style: TextStyle(color: Colors.white),),
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                        color: Colors.deepOrange
+                        ),
+                        ),
+                      child: Image.network(item.image),
+                      footer: Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                        color: Colors.deepOrange
+                        ),
+                        child: Text("\$"+item.price.toString(), style: TextStyle(color: Colors.white),)),
+                      ));
+                })
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
