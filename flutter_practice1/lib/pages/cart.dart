a53774999a2c18795f0bb69d95a74639571bb900
aprashant21 <aprashant21@tbc.edu.np>
2022-05-13 17:58:14 +0545

    cart page created

--- flutter_practice1/lib/pages/cart.dart
+++ flutter_practice1/lib/pages/cart.dart
@@ -3,17 +3,69 @@ import 'package:flutter_practice1/widgets/theme.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CartPage extends StatelessWidget {
-  const CartPage({ Key? key }) : super(key: key);
+  const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        title:"My Cart".text.make(),
+        title: "My Cart".text.make(),
       ),
       backgroundColor: context.canvasColor,
-      
+      body: Column(
+        children: [
+          _CartList().p32().expand(),
+          Divider(),
+          _CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$999".text.xl5.color(context.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+             style: ButtonStyle(
+               backgroundColor: MaterialStateProperty.all(context.theme.buttonColor)
+             ),
+             child: "Buy".text.white.make()
+             ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+
+class _CartList extends StatefulWidget {
+  @override
+  State<_CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<_CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount:5,
+      itemBuilder: (context,index)=> ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: (){},
+      ),
+      title:"Item 1".text.make()
+    )
     );
   }
 }
