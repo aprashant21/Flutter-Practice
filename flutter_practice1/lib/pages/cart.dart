019c5f693996becf8757f745837ffc530115bc83
aprashant21 <aprashant21@tbc.edu.np>
2022-05-25 18:29:07 +0545

    RemoveMutation, total Price using VxConsumer

--- flutter_practice1/lib/pages/cart.dart
+++ flutter_practice1/lib/pages/cart.dart
@@ -36,7 +36,9 @@ class _CartTotal extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          "\$${_cart.totalPrice}".text.xl5.color(context.accentColor).make(),
+          VxConsumer(builder: (context,dynamic,status){
+            return "\$${_cart.totalPrice}".text.xl5.color(context.accentColor).make();
+          }, mutations: {RemoveMutation}, notifications: {}),
           30.widthBox,
           ElevatedButton(
                   onPressed: () {
@@ -55,9 +57,10 @@ class _CartTotal extends StatelessWidget {
   }
 }
 
-class _CartList extends StatelessWidget{
+class _CartList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    VxState.watch(context, on: [RemoveMutation]);
     final CartModel _cart = (VxState.store as MyStore).cart;
     return _cart.items.isEmpty
         ? "Nothing to show".text.xl3.makeCentered()
@@ -67,12 +70,7 @@ class _CartList extends StatelessWidget{
                 leading: Icon(Icons.done),
                 trailing: IconButton(
                   icon: Icon(Icons.remove_circle_outline),
-                  onPressed: () {
-                    _cart.remove(_cart.items[index]);
-                    // setState(() {
-                      
-                    // });
-                  },
+                  onPressed: () => RemoveMutation(_cart.items[index]),
                 ),
                 title: _cart.items[index].name.text.make()));
   }
