576ca27b01daa4adafefefaad8544b17b4205f2e
aprashant21 <aprashant21@tbc.edu.np>
2022-05-25 18:12:46 +0545

    add Mutation from VxStore and fix add to cart

--- flutter_practice1/lib/models/cart.dart
+++ flutter_practice1/lib/models/cart.dart
@@ -1,9 +1,10 @@
+import 'package:flutter_practice1/core/store.dart';
 import 'package:flutter_practice1/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
   //catalog field
   late CatalogModel _catalog;
-
 
   //collection of IDs - Store Ids of each item
   final List<int> _itemIds = [];
@@ -21,13 +22,19 @@ class CartModel {
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
 
-  //add Item
-  void add(Item item) {
-    _itemIds.add(item.id);
-  }
-
 //remove Item
   void remove(Item item) {
     _itemIds.remove(item.id);
+  }
+}
+
+class AddMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutation(this.item);
+
+  @override
+  perform() {
+    store?.cart._itemIds.add(item.id);
   }
 }
--- flutter_practice1/lib/widgets/home_widgets.dart/add_to_cart.dart
+++ flutter_practice1/lib/widgets/home_widgets.dart/add_to_cart.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
+import '../../core/store.dart';
 import '../../models/cart.dart';
 import '../../models/catalog.dart';
 
 class AddToCart extends StatelessWidget {
   final Item catalog;
-   AddToCart({
+  AddToCart({
     Key? key,
     required this.catalog,
   }) : super(key: key);
 
-  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
+    VxState.watch(context,on:[AddMutation]);
+    final CartModel _cart = (VxState.store as MyStore).cart;
     bool isInCart = _cart.items.contains(catalog) ?? false;
     return ElevatedButton(
       onPressed: () {
         if (!isInCart) {
-          isInCart = isInCart.toggle();
-          final _catalog = CatalogModel();
-          _cart.catalog = _catalog;
-          _cart.add(catalog);
-          // setState(() {});
+          AddMutation(catalog);
         }
       },
       child: isInCart ? Icon(Icons.done) : Icon(CupertinoIcons.cart_badge_plus),
